51a536f2b24f4e9cc7710b5bdf91aa94d08c298b
Marcelo <marceloe.fuentes@gmail.com>
2018-04-26 15:37:41 -0300

    apply editorconfig rules

--- src/extension.ts
+++ src/extension.ts
@@ -4,43 +4,46 @@ import * as vscode from 'vscode';
 import Linter, { LinterError, LinterHandler } from './linter';
 
 
-function doLint(document: vscode.TextDocument, diagnosticCollection: vscode.DiagnosticCollection): LinterHandler {
-    return function(errors: LinterError[]) {
-        diagnosticCollection.clear();
-        const diagnostics = errors.map(error => {
-            const range = new vscode.Range(
-                new vscode.Position(error.line - 1, error.range.start - 1),
-                new vscode.Position(error.line - 1, error.range.end - 1)
-            );
-            
-            return new vscode.Diagnostic(range, error.reason, vscode.DiagnosticSeverity.Warning);
-        });
+function doLint(
+  document: vscode.TextDocument,
+  diagnosticCollection: vscode.DiagnosticCollection
+): LinterHandler {
+  return function(errors: LinterError[]) {
+    diagnosticCollection.clear();
+    const diagnostics = errors.map(error => {
+      const range = new vscode.Range(
+        new vscode.Position(error.line - 1, error.range.start - 1),
+        new vscode.Position(error.line - 1, error.range.end - 1)
+      );
 
-        diagnosticCollection.set(document.uri, diagnostics);
-    };
+      return new vscode.Diagnostic(range, error.reason, vscode.DiagnosticSeverity.Warning);
+    });
+
+    diagnosticCollection.set(document.uri, diagnostics);
+  };
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    const diagnosticCollection = vscode.languages.createDiagnosticCollection("protoc-gen-lint");
+  const diagnosticCollection = vscode.languages.createDiagnosticCollection("protoc-gen-lint");
 
-    let disposable = vscode.commands.registerCommand('extension.protolint', () => {
-        const linter = new Linter();
-        
-        vscode.workspace.onDidSaveTextDocument(linter.lint);
-        vscode.workspace.onDidOpenTextDocument(function() {
-            const editor = vscode.window.activeTextEditor;
-            if (editor) {
-                linter.lint(editor.document, doLint(editor.document, diagnosticCollection), true);
-            }
-        });
+  let disposable = vscode.commands.registerCommand('extension.protolint', () => {
+    const linter = new Linter();
 
-        vscode.workspace.onWillSaveTextDocument(function(event: vscode.TextDocumentWillSaveEvent) {
-            linter.lint(event.document, doLint(event.document, diagnosticCollection));
-        });
+    vscode.workspace.onDidSaveTextDocument(linter.lint);
+    vscode.workspace.onDidOpenTextDocument(function() {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+          linter.lint(editor.document, doLint(editor.document, diagnosticCollection), true);
+      }
     });
 
-    vscode.commands.executeCommand("extension.protolint");
-    context.subscriptions.push(disposable);
+    vscode.workspace.onWillSaveTextDocument(function(event: vscode.TextDocumentWillSaveEvent) {
+      linter.lint(event.document, doLint(event.document, diagnosticCollection));
+    });
+  });
+
+  vscode.commands.executeCommand("extension.protolint");
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
